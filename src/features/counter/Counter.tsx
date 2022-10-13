4870c8b25be564a0b94e21d754d01838fceb172e
weimeng <xu13071061935@163.com>
2022-10-13 22:59:22 +0800

    tijiao

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -26,7 +26,7 @@ export function Counter() {
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
-          -
+                -
         </button>
         <span className={styles.value}>{count}</span>
         <button
